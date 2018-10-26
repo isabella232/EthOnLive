8dc8a89a8119ed52fb68bc454aa13d491efdd64d
Johannes <jo-tud@users.noreply.github.com>
2018-10-26 17:21:56 +0200

    type sesame

--- js/lodlive.profile.js
+++ js/lodlive.profile.js
@@ -12,7 +12,8 @@ $.jStorage.set('profile', {
 			},
 			useForInverseSameAs : true,
 			/*change this*/
-			endpoint : 'https://linkeddata.aleth.io/sparql',
+			endpoint : 'https://linkeddata.aleth.io/sparql',,
+			endpointType : 'sesame',
 			examples : [{
 				label : 'Block 6575377',
 				uri : 'http://ethereum.ethstats.io/Block_6575377'
